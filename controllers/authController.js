286eab051611cf80cb5d72f6567b466acf92bb33
Bastian1212 <bastianvillanueva@me.com>
2022-08-24 14:42:47 -0400

    auth terminado

---
+++ controllers/authController.js
@@ -0,0 +1,58 @@
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+
+
+exports.autenticarUsuario = async (req, res )  => {
+    // revisar si hay errores 
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors.array()})
+    }
+
+    const {email , password } = req.body;
+
+    try{
+        // Revisar si el usuario esta registrado
+
+        let usuario = await Usuario.findOne({email});
+        if(!usuario){
+            return res.status(400).json({msg : "El usuario no existe"});
+        }
+
+        // revisar clave 
+        const passCorrecto = await bcryptjs.compare(password, usuario.password);
+        if(!passCorrecto){
+            return res.status(400).json({msg : "PassWord Incorrecto"});
+        }
+
+        //  todo ok 
+
+         // Crear y firmar el jwt
+        const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+
+        // firnmar el JWT 
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 // 1 hora
+        }, (error, token) => {
+            if(error) throw error;
+
+            // Mensaje de confirmación
+            res.json({ token  });
+        });
+
+
+    }catch(error){
+        console.log(error);
+
+    }
+    
+
+
+}
---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+//Rutas para autenticar llos usuarios 
+const express = require("express");
+const router = express.Router();
+const {check} = require("express-validator");
+
+const authController = require("../controllers/authController");
+
+// crea un usuario 
+
+// api/auth
+
+router.post("/" ,
+    [
+        check("email", "Agrega Un Email  Valido ").isEmail(),
+        check("password", "El password debe ser minimo de 6 caracteres ").isLength({min: 6 })
+    ],
+    authController.autenticarUsuario
+
+);
+
+module.exports = router; 
+
