09f0aee793fcf45c0eb22c1e87f43f39e4d19f05
Bastian1212 <bastianvillanueva@me.com>
2022-08-24 12:24:19 -0400

    avance de los registro de usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,18 +1,30 @@
 const Usuario = require("../models/Usuario");
-
+const bcryptjs = require("bcryptjs");
+const {validationResult} = require ("express-validator");
 exports.crearUsuario = async(req,res) => {
+    // revisar si hay errores 
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors.array()})
+    }
+
 
     //extrar email y password 
     const {email,  password} = req.body;
 
     try{
-
+        // revisar que el usuario registrado sea unico 
         let usuario = await Usuario.findOne({email});
         if(usuario){
             return res.status(400).json({msg: "El usuario ya existe "})
         }
         // crea el nuevo usuario 
         usuario = new Usuario(req.body);
+
+        //Hashear el password 
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt);
+
 
         // guarda el nuevo usuario 
         await usuario.save();
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,12 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 const usuarioController = require("../controllers/usuarioController")
+const {check} = require("express-validator");
 
 // crea un usuario 
 
 // api/usuarios
 
 router.post("/" ,
+    [
+        check("nombre", "El nombre es obligatorio").not().isEmpty(),
+        check("email", "Agrega Un Email  Valido ").isEmail(),
+        check("password", "El password debe ser minimo de 6 caracteres ").isLength({min: 6 })
+    ],
     usuarioController.crearUsuario
 
 );
