e430faba6bdf79f2e6b356cdc9499d1ffdaf1779
Bastian1212 <bastianvillanueva@me.com>
2022-09-02 10:50:49 -0400

    ajustes

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -2,7 +2,7 @@ const Tarea = require("../models/Tarea");
 const Proyecto = require("../models/Proyecto");
 const {validationResult} = require("express-validator");
 
-
+//
  // Crea una nueva tarea 
 
 exports.crearTarea = async (req, res ) => {
